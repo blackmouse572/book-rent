96e29a771cd0a5e4b066b0c6428a961bcf277137
blackmouse572 <77144199+blackmouse572@users.noreply.github.com>
2023-09-25 19:20:48 +0700

    Feature/auth pages (#1)
    * feat: add packages and user type
    
    * feat: add back button
    
    * feat: add `secondary` colors
    
    * feat: add auth provider hook
    
    * feat: add auth validators
    
    * feat: implement auth provider
    
    * feat: add separator and form
    
    * feat: add auth form and screen
    
    * feat: add demo profile page
    
    * fix: faker properties registration

--- src/type/user.d.ts
+++ src/type/user.d.ts
@@ -1,13 +1,13 @@
 export interface User {
-  _id: string;
-  email: string;
-  fullName: string;
-  role: string;
-  username: string;
-  blocked: boolean = false;
-  address?: string | null;
-  password?: string;
-  avatar?: string;
-  createdAt: Date;
-  updatedAt: Date;
+    _id: string;
+    email: string;
+    fullName: string;
+    role: string;
+    username: string;
+    blocked: boolean = false;
+    address?: string | null;
+    password?: string;
+    avatar?: string;
+    createdAt: Date;
+    updatedAt: Date;
 }
