d9e062e1c60444a16d8487ec2ddba5ceebfb89ff
Jaden Nguyen <jaden.nguyen@avepoint.com>
2023-10-04 17:33:18 +0700

    feat: update user to match api

--- src/type/user.d.ts
+++ src/type/user.d.ts
@@ -1,8 +1,11 @@
 export interface User {
-  id: string;
+  _id: string;
   email: string;
   fullName: string;
   role: string;
+  username: string;
+  blocked: boolean = false;
+  address?: string | null;
   password?: string;
   avatar?: string;
   createdAt: Date;
