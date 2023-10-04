7131da8116e93b702b74005316cb52c5032acae4
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-10-04 22:07:54 +0700

    feat: add token to useAuth

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,49 +1,62 @@
 // AuthContext.tsx
 
-import React, { createContext, useContext, useEffect, useState } from 'react';
-import { User } from '../type';
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { User } from "../type";
 
 interface AuthContextType {
-  user: User | null;
-  login: (user: User) => void;
-  logout: () => void;
+    user: User | null;
+    token: string | null;
+    login: (user: User) => void;
+    logout: () => void;
+    setAccessToken: (token: string) => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: React.PropsWithChildren) => {
-  const [user, setUser] = useState<User | null>(null);
-  // Load user from localStorage on initial render
-  useEffect(() => {
-    const savedUser = localStorage.getItem('user');
-    if (savedUser) {
-      setUser(JSON.parse(savedUser));
-    }
-  }, []);
+    const [user, setUser] = useState<User | null>(null);
+    const [token, setToken] = useState<string | null>(null);
+    // Load user from localStorage on initial render
+    useEffect(() => {
+        const savedUser = localStorage.getItem("user");
+        if (savedUser) {
+            setUser(JSON.parse(savedUser));
+        }
+    }, []);
 
-  const login = (user: User) => {
-    setUser(user);
-    localStorage.setItem('user', JSON.stringify(user));
-  };
+    const login = (user: User) => {
+        setUser(user);
+        localStorage.setItem("user", JSON.stringify(user));
+    };
 
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem('user');
-  };
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("user");
+    };
 
-  const authContextValue: AuthContextType = {
-    user,
-    login,
-    logout,
-  };
+    const setAccessToken = (token: string) => {
+        setToken(token);
+    };
 
-  return <AuthContext.Provider value={authContextValue}>{children}</AuthContext.Provider>;
+    const authContextValue: AuthContextType = {
+        user,
+        login,
+        logout,
+        setAccessToken,
+        token,
+    };
+
+    return (
+        <AuthContext.Provider value={authContextValue}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export const useAuth = (): AuthContextType => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-  return context;
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
 };
