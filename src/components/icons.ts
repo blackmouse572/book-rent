78410a83d859962c842040a8d54fd2830248e72e
Jaden Nguyen <jaden.nguyen@avepoint.com>
2023-10-04 14:57:03 +0700

    feat: add icon component

---
+++ src/components/icons.ts
@@ -0,0 +1,16 @@
+import {
+  type Icon as TablerIcons,
+  IconVocabulary,
+  IconX,
+  IconSquareRoundedXFilled,
+  IconBrandGoogleHome,
+} from "@tabler/icons-react";
+
+export type Icon = TablerIcons;
+
+export const Icons = {
+  logo: IconVocabulary,
+  close: IconX,
+  squareClose: IconSquareRoundedXFilled,
+  home: IconBrandGoogleHome,
+};
--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,5 +1,5 @@
-import { Outlet } from 'react-router-dom';
-import TailwindIndicator from '../components/Tailwind-Indicator';
+import { Outlet } from "react-router-dom";
+import TailwindIndicator from "../components/Tailwind-Indicator";
 
 function MainLayout() {
   return (
