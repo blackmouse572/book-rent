f1b2e9140abd07d2c44563028c4bae7ac5edac5b
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-10-04 22:20:49 +0700

    fix: return void to profile api callback

--- src/apis/auth/apis/profile.api.ts
+++ src/apis/auth/apis/profile.api.ts
@@ -1,28 +1,27 @@
 import { axiosClient } from "@/lib/axios";
-import { AxiosError } from "axios";
-import { ITokenReponse } from "../types/token";
-import { IProfileResponse } from "../types/user-response";
 import { User } from "@/type";
+import { AxiosError } from "axios";
+import { IProfileResponse } from "../types/user-response";
 
 async function profileApi(
-  token: string,
-  callback: (error: AxiosError | null, result: User | null) => any
+    token: string,
+    callback: (error: AxiosError | null, result: User | null) => void
 ) {
-  return await axiosClient
-    .get<IProfileResponse>("/user/profile", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    })
-    .then((err) => {
-      if (err.status === 200) {
-        callback(null, err.data.data);
-      }
-    })
-    .catch((error) => {
-      callback(error, null);
-    });
+    return await axiosClient
+        .get<IProfileResponse>("/user/profile", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+        })
+        .then((err) => {
+            if (err.status === 200) {
+                callback(null, err.data.data);
+            }
+        })
+        .catch((error) => {
+            callback(error, null);
+        });
 }
 
 export { profileApi };
