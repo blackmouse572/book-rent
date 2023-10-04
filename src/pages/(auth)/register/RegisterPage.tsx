e1cba660db6192c68b7f9d8145af14817b662001
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-10-04 22:42:01 +0700

    feat: register

--- src/pages/(auth)/register/RegisterPage.tsx
+++ src/pages/(auth)/register/RegisterPage.tsx
@@ -1,13 +1,13 @@
-import HomeButtton from '../../../components/Home-Button';
-import RegisterForm from '../../../components/Register-Form';
+import HomeButtton from "../../../components/Home-Button";
+import RegisterForm from "../../../components/Register-Form";
 
 function RegisterPage() {
-  return (
-    <div className="container mx-auto flex justify-center items-center h-screen relative">
-      <HomeButtton className="absolute top-0 left-0 mt-4 ml-4" />
-      <RegisterForm />
-    </div>
-  );
+    return (
+        <div className="container mx-auto flex justify-center items-center min-h-screen relativei ">
+            <HomeButtton className="absolute top-0 left-0 mt-4 ml-4" />
+            <RegisterForm />
+        </div>
+    );
 }
 
 export default RegisterPage;
