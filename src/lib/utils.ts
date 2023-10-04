623a6acc1f847482011c303d4f42d9d7d4eb331a
Jaden Nguyen <jaden.nguyen@avepoint.com>
2023-10-04 17:34:59 +0700

    refactor: formating

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getLabelByFullname(fullName: string): string {
+  const split = fullName.split(" ");
+  const firstLetter = split.map((word) => word.charAt(0).toUpperCase());
+
+  return firstLetter.join("");
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,19 @@
-import { Badge } from '../components/ui/badge';
-import { Button } from '../components/ui/button';
-import { Input } from '../components/ui/input';
-import { Label } from '../components/ui/label';
-import { toast } from '../components/ui/use-toast';
+import { Separator } from "@/components/ui/separator";
+import { Badge } from "../components/ui/badge";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
+import { toast } from "../components/ui/use-toast";
+import { Avatar } from "@/components/ui/avatar";
+import AuthPreview from "@/components/AuthPreview";
 
 function HomePage() {
-  const showToast = (variant: 'default' | 'destructive' | 'success' | 'warning') => {
+  const showToast = (
+    variant: "default" | "destructive" | "success" | "warning"
+  ) => {
     toast({
       title: variant,
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
       variant,
     });
   };
@@ -16,24 +21,24 @@ function HomePage() {
     <div className="container h-screen flex-col  flex gap-8 justify-center items-cente">
       <div className=" flex gap-3 justify-center items-center">
         <Button>Primary</Button>
-        <Button variant={'ghost'}>Ghost</Button>
-        <Button variant={'link'}>Link</Button>
-        <Button variant={'outline'}>Outline</Button>
-        <Button variant={'secondary'}>Secondary</Button>
+        <Button variant={"ghost"}>Ghost</Button>
+        <Button variant={"link"}>Link</Button>
+        <Button variant={"outline"}>Outline</Button>
+        <Button variant={"secondary"}>Secondary</Button>
       </div>
       <div className="max-w-sm flex gap-3 justify-center items-center mx-auto">
         <Label htmlFor="test">Test</Label>
         <Input id="test" />
       </div>
       <div className=" flex gap-3 justify-center items-center">
-        <Button onClick={() => showToast('default')}>Default Toast</Button>
-        <Button onClick={() => showToast('warning')} colors="warning">
+        <Button onClick={() => showToast("default")}>Default Toast</Button>
+        <Button onClick={() => showToast("warning")} colors="warning">
           Warning Toast
         </Button>
-        <Button onClick={() => showToast('destructive')} colors="destructive">
+        <Button onClick={() => showToast("destructive")} colors="destructive">
           Destructive Toast
         </Button>
-        <Button onClick={() => showToast('success')} colors="success">
+        <Button onClick={() => showToast("success")} colors="success">
           Success Toast
         </Button>
       </div>
@@ -42,19 +47,21 @@ function HomePage() {
           <span>Default</span>
         </Badge>
         <Badge isPressable>Presable</Badge>
-        <Badge variant={'outline'}>
+        <Badge variant={"outline"}>
           <span>Outline</span>
         </Badge>
-        <Badge colors={'secondary'}>
+        <Badge colors={"secondary"}>
           <span>Secondary</span>
         </Badge>
-        <Badge isPressable colors={'warning'}>
+        <Badge isPressable colors={"warning"}>
           Presable warning
         </Badge>
-        <Badge variant={'outline'} colors={'destructive'}>
+        <Badge variant={"outline"} colors={"destructive"}>
           <span>Destructive</span>
         </Badge>
       </div>
+      <Separator />
+      <AuthPreview />
     </div>
   );
 }
