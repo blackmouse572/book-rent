da54dc22d6b19126d0db4f3ad5d6911919096615
blackmouse572 <77144199+blackmouse572@users.noreply.github.com>
2023-10-06 19:19:33 +0700

    Feature/auth api (#4)
    * feat: add user login
    
    * feat: update user to match api
    
    * feat: add auth preview to display login state
    
    * feat: add icon component
    
    * refactor: formating
    
    * feat: config prettierc
    
    * feat: add token to useAuth
    
    * fix: return void to profile api callback
    
    * feat: register
    
    * feat: refresh-token
    
    * feat: add isLoading props to button
    
    * Feature/auth api (#2)
    
    * Feature/auth pages (#1)
    
    * feat: add packages and user type
    
    * feat: add back button
    
    * feat: add `secondary` colors
    
    * feat: add auth provider hook
    
    * feat: add auth validators
    
    * feat: implement auth provider
    
    * feat: add separator and form
    
    * feat: add auth form and screen
    
    * feat: add demo profile page
    
    * fix: faker properties registration
    
    * feat: add icon component
    
    * feat: add user login
    
    * feat: update user to match api
    
    * feat: add auth preview to display login state
    
    * feat: add icon component
    
    * refactor: formating
    
    * feat: config prettierc
    
    * feat: add token to useAuth
    
    * fix: return void to profile api callback
    
    * feat: register
    
    * feat: refresh-token
    
    * feat: add isLoading props to button
    
    ---------
    
    Co-authored-by: Jaden Nguyen <jaden.nguyen@avepoint.com>
    
    * feat: implement button isLoading
    
    * refactor: square checkbox instead of rounded
    
    * refactor: toast now dimiss faster
    
    ---------
    
    Co-authored-by: Jaden Nguyen <jaden.nguyen@avepoint.com>

--- src/pages/(auth)/login/validation.ts
+++ src/pages/(auth)/login/validation.ts
@@ -1,8 +1,8 @@
 import * as z from "zod";
 // export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/; // 8 characters, 1 uppercase, 1 lowercase, 1 number
 export const LoginSchema = z.object({
-    userNameOrEmail: z.string().min(3),
-    password: z.string().min(6),
-    // .regex(passwordRegex, 'Password must contain 8 characters, 1 uppercase, 1 lowercase, 1 number')
-    remember: z.boolean(),
+  userNameOrEmail: z.string().min(3),
+  password: z.string().min(6),
+  // .regex(passwordRegex, 'Password must contain 8 characters, 1 uppercase, 1 lowercase, 1 number')
+  remember: z.boolean(),
 });
