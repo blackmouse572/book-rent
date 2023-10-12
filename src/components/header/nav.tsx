e2ebbc230ebeadce9de503c101ede56329015b40
allforest01 <allforest01@gmail.com>
2023-10-12 19:32:13 +0700

    Header & Nav

---
+++ src/components/header/nav.tsx
@@ -0,0 +1,37 @@
+import AuthPreview from "../AuthPreview";
+
+export default function NavBar() {
+    return (
+        <div className="navbar-wrapper container-wrapper">
+            <nav className="container navbar flex flex-row items-center justify-between">
+                <div className="flex v-center R8eLNj">
+                    <svg className="w-6 h-6 text-gray-800 dark:text-white mr-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                    </svg>
+                    <span>Can we help you?</span>
+                    <span className="ml-5">+1 246-345-0695</span>
+                </div>
+                <ul className="navbar__links flex space-x-4">
+                    <li>
+                        <a className='' href='#'>
+                            <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 21">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 3.464V1.1m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175C15 15.4 15 16 14.462 16H1.538C1 16 1 15.4 1 14.807c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 8 3.464ZM4.54 16a3.48 3.48 0 0 0 6.92 0H4.54Z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a className='' href='#'>
+                            <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    {/* <li>
+                        <AuthPreview/>
+                    </li> */}
+                </ul>
+            </nav>
+            <hr className="border-gray-200 sm:mx-auto dark:border-gray-700" />
+        </div>
+    );
+}
