c5dbb550131172a3ed3799c86cd6cb2a1b98acec
allforest01 <allforest01@gmail.com>
2023-10-13 23:23:29 +0700

    Combine Header vs Nav

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,36 +1,132 @@
 import React from 'react';
-import '@/components/header/header.css';
+import '@/index.css';
+import AuthPreview from '../AuthPreview';
 
-
-export default function Header() {
+function Header() {
   return (
-        <div className="flex h-screen items-center">
-        <header className="bg-white-500 flex h-24 w-full items-center justify-around">
-            <div className="logo">
-                <h1 className="text-black-100 cursor-pointer text-3xl font-extrabold xs:text-2xl">BRental</h1>
-            </div>
-            <nav className="w-1/2 xs:hidden">
-                <div className="flex items-center justify-evenly">
-                    <a href="#" className="active">Home</a>
-                    <a href="#">Category</a>
-                    <a href="#">About Us</a>
-                </div>
-            </nav>
-            <form>
-                <label htmlFor="default-search" className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white">Search</label>
-                <div className="relative">
-                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg className="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+    <header>
+      <div className="navbar-wrapper container-wrapper">
+        <nav className="container navbar flex flex-row items-center justify-between p-0">
+          <div className="flex v-center R8eLNj">
+            <svg
+              className="w-6 h-6 text-gray-800 dark:text-white mr-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <span>Can we help you?</span>
+            <span className="ml-5">+1 246-345-0695</span>
+          </div>
+          <ul className="navbar__links flex space-x-4">
+            <li>
+              <a className="" href="#">
+                <svg
+                  className="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 16 21"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M8 3.464V1.1m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175C15 15.4 15 16 14.462 16H1.538C1 16 1 15.4 1 14.807c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 8 3.464ZM4.54 16a3.48 3.48 0 0 0 6.92 0H4.54Z"
+                  />
+                </svg>
+              </a>
+            </li>
+            <li>
+                <a className='' href='#'>
+                    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
                     </svg>
-                    </div>
-                    <input type="search" id="default-search" className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-12 pr-60 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" placeholder="Search by Keywords" required />
-                </div>
-            </form>
-            <svg className="w-6 h-6 text-gray-800 dark:text-white mr-20 ml-10" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
-            </svg>  
-        </header>
+                </a>
+            </li>
+            {/* <li>
+                <AuthPreview/>
+            </li> */}
+          </ul>
+        </nav>
+        <hr className="border-gray-200 sm:mx-auto dark:border-gray-700" />
+      </div>
+      <div className="flex items-center pl-0">
+        <div className="bg-white-500 flex h-24 w-full items-center justify-around">
+          <div className="logo">
+            <h1 className="text-black-100 cursor-pointer text-3xl font-extrabold xs:text-2xl">BRental</h1>
+          </div>
+          <nav className="w-1/2 xs:hidden">
+            <div className="flex items-center justify-evenly">
+              <a href="#" className="active">
+                Home
+              </a>
+              <a href="#">Category</a>
+              <a href="#">About Us</a>
+            </div>
+          </nav>
+          <form>
+            <label
+              htmlFor="default-search"
+              className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg
+                  className="h-4 w-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-12 pr-60 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                placeholder="Search by Keywords"
+                required
+              />
+            </div>
+          </form>
+          <svg
+            className="w-6 h-6 text-gray-800 dark:text-white mr-20 ml-10"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"
+            />
+          </svg>
         </div>
+      </div>
+    </header>
   );
 }
+
+export default Header;
