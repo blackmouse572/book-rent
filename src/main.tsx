2bad2a40979b8d2424cd6fc0b7689daccf0d7572
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-10-13 23:00:20 +0700

    Hotfix: wrap suspend for lazy load

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./components/auth-provider";
+import PageLoader from "./components/page-loader";
 import { Toaster } from "./components/ui/toaster";
 import "./index.css";
 import { queryClient } from "./lib/query";
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <AuthProvider>
+                <Suspense fallback={<PageLoader/>} >
                 <RouterProvider router={ROUTES} />
+                </Suspense>
             </AuthProvider>
             <Toaster />
         </QueryClientProvider>
