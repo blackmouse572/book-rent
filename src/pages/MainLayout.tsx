6c35d985de0464af2aa992d42dcf72ba81a16d1d
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-10-21 15:30:04 +0700

    feat: default title header for all page inside MainLayout

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/footer/Footer";
 import NavBar from "@/components/header/nav";
+import MetaData from "@/components/metadata";
 import { Outlet } from "react-router-dom";
 import TailwindIndicator from "../components/Tailwind-Indicator";
 
 function MainLayout() {
     return (
         <div className="min-h-screen bg-background font-sans antialiased">
+            <MetaData title="Homepage" />
             <NavBar />
             <TailwindIndicator />
             <Outlet />
