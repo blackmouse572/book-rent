d0340eb82a9d5abb16484c58ac2611267b90781f
Quốc <anhquockkzz@gmail.com>
2023-10-24 10:10:37 +0700

    refactor:infoaccount

--- src/pages/(profile)/InfoAccount.tsx
+++ src/pages/(profile)/InfoAccount.tsx
@@ -2,8 +2,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
-// Import các component từ thư viện UI của bạn
-// Thay thế các import này bằng import thực tế của bạn
 import { Button } from "@/components/ui/button";
 import {
     Form,
