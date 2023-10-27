097bac6b9a3da10ebc42588006699b6b089cfc35
Quốc <anhquockkzz@gmail.com>
2023-10-27 07:58:15 +0700

    refactor:inforaccount, changpassword

--- src/pages/(auth)/ChangePassword.tsx
+++ src/pages/(auth)/ChangePassword.tsx
@@ -34,55 +34,41 @@ export function ChangePassword() {
     setIsFormSubmitted(true);
   };
 
-    return (
-        <div className="max-w-md mx-auto m-4 p-6 bg-white rounded-md shadow-md">
-            <h2 className="text-2xl font-bold mb-4">Change Password</h2>
-            <form className="space-y-4">
-                <div>
-                    <label htmlFor="currentPassword" className="block text-gray-700">
-                        Current Password
-                    </label>
-                    <input
-                        type="password"
-                        id="currentPassword"
-                        // value={currentPassword}
-                        // onChange={(e) => setCurrentPassword(e.target.value)}
-                        className="block w-full px-4 py-2 mt-2 text-gray-700 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                    />
-                </div>
-                <div>
-                    <label htmlFor="newPassword" className="block text-gray-700">
-                        New Password
-                    </label>
-                    <input
-                        type="password"
-                        id="newPassword"
-                        // value={newPassword}
-                        // onChange={(e) => setNewPassword(e.target.value)}
-                        className="block w-full px-4 py-2 mt-2 text-gray-700 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                    />
-                </div>
-                <div>
-                    <label htmlFor="confirmPassword" className="block text-gray-700">
-                        Confirm New Password
-                    </label>
-                    <input
-                        type="password"
-                        id="confirmPassword"
-                        // value={confirmPassword}
-                        // onChange={(e) => setConfirmPassword(e.target.value)}
-                        className="block w-full px-4 py-2 mt-2 text-gray-700 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring"
-                >
-                    Change Password
-                </button>
-            </form>
-        </div>
-    );
-};
-
-export default ChangePasswordForm;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-md mx-auto">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="block text-gray-700 text-sm font-bold mb-2">
+                Username
+              </FormLabel>
+              <FormControl>
+                <Input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Enter your username"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="text-gray-600 text-xs italic mt-1">
+                Please enter your username to reset your password.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+         
+        >
+          Submit
+        </Button>
+        {isFormSubmitted && (
+          <p className="text-green-500 mt-2 text-sm">Reset password link sent to your email.</p>
+        )}
+      </form>
+    </Form>
+  );
+}
