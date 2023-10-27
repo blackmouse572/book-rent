273ea4d50701531ca3b6cac58c6e268522556ec4
Quốc <anhquockkzz@gmail.com>
2023-10-27 17:52:42 +0700

    refactor:header, routes, css changpassword, infoaccount

--- src/pages/(auth)/ChangePassword.tsx
+++ src/pages/(auth)/ChangePassword.tsx
@@ -36,35 +36,47 @@ function ChangePassword() {
     };
 
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input
-                                    placeholder="Enter your username"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormDescription>
-                                Please enter your username to reset your
-                                password.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
+        <div className="container mx-auto flex justify-center items-center h-screen relative">
+            <HomeButtton className="absolute top-0 left-0 mt-4 ml-4" />
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8 max-w-md mx-auto "
+                >
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className=" mb-2">
+                                    Username
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        className=" py-2 px-3 "
+                                        placeholder="Enter your username"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormDescription className="mt-1">
+                                    Please enter your username to reset your
+                                    password.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className=" py-2 px-4 ">
+                        Submit
+                    </Button>
+                    {isFormSubmitted && (
+                        <p className="text-green-500 mt-2 text-sm">
+                            Reset password link sent to your email.
+                        </p>
                     )}
-                />
-                <Button type="submit">Submit</Button>
-                {isFormSubmitted && (
-                    <p className="text-green-500 mt-2 text-sm">
-                        Reset password link sent to your email.
-                    </p>
-                )}
-            </form>
-        </Form>
+                </form>
+            </Form>
+        </div>
     );
 }
+export default ChangePassword;
