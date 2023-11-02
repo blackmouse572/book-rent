28b9f602d6faf9cfdf85edc5ec97334856ea07fa
allforest01 <allforest01@gmail.com>
2023-11-02 17:00:56 +0700

    refator: update icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,3 +1,4 @@
+import { IconProps } from "@radix-ui/react-icons/dist/types";
 import {
     IconBell,
     IconBookmarkQuestion,
@@ -65,5 +66,20 @@ export const Icons = {
     arrowLeft: IconArrowLeft,
     done: IconDiscountCheckFilled,
     fail: IconMoodSadSquint,
-    loading: IconTruckLoading
+    loading: IconTruckLoading,
+    sidebar: (props: IconProps) => ( <svg
+        className="w-6 h-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h16"
+        />
+    </svg>),
 };
