fec5ef03eeacb1df16aa40672983c86c7ae79f70
Jaden Nguyen <jaden.nguyen@avepoint.com>
2023-11-09 16:21:56 +0700

    fix: use default export for IReview

--- src/types/review.ts
+++ src/types/review.ts
@@ -1,6 +1,6 @@
 import { User } from "@/types";
 
-export interface IReview {
+export default interface IReview {
     _id: string;
     author: Pick<User, "_id" | "email" | "avatar" | "fullName">;
     comment: string;
