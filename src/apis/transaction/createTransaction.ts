b4a5f2574f26e8aed5d28c0b369eb6c26a5166af
SaNgoTin <phuoctungpbc02@gmail.com>
2023-11-12 08:19:34 +0700

    fix: update logic before call api (#36)

--- src/apis/transaction/createTransaction.ts
+++ src/apis/transaction/createTransaction.ts
@@ -2,7 +2,7 @@ import { authAxiosClient } from "@/lib/axios";
 
 export async function postTransactionApi(orderId: string, amount: string) {
     return await authAxiosClient
-        .post("/transaction", { orderId, amount: +amount }, {})
+        .post("/transaction", { orderId, amount: +amount / 100 }, {})
         .then((response) => {
             if (response.status == 201) {
                 return response.data;
